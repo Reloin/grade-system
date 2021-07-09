ce40b54ac9eb15eea65c4bbcafd6619350ff28c0
Leong <yihengleong@gmail.com>
2021-07-09 13:18:37 +0800

    Added student dob

--- base.cpp
+++ base.cpp
@@ -3,50 +3,43 @@
 #include <cstring>
 #include <fstream>
 #include <sys/stat.h>
-#include <unistd.h>
 
 using namespace std;
 
 
 //-----------------------time类，用于记录出勤的日期----------------------------------------------
-class classTime
+class date
 {
     protected:
-        int week;//Sun = 0, Mon = 1 ...
-    private:
-        int year;
-        int month;
-        int day;
-        int period;
-        void setWeek(){
-            //月份1月为0以此类推，年份是从1900年到今天
-            std::tm time_in = { 0, 0, 0, day, month - 1, year - 1900};
-            std::time_t time_temp = std::mktime(&time_in);
-            const std::tm *time_out = std::localtime(&time_temp);
-            cout << time_out->tm_wday << endl;
-            week = time_out->tm_wday;
-            cout << week;
-        }
+        int year, month, day;
     public:
-        classTime(int p, int d, int m, int y): period(p), day(d), month(m), year(y)
+        date(int y = 2000, int m = 1, int d = 1): year(y), month(m), day(d){}
+        date(const date &d):year(d.year), month(d.month), day(d.day){}
+        string dob()//以yyyy-mm-dd形式显示生日
         {
-            setWeek();
+            char temp[11];
+            sprintf(temp, "%d-%d-%d", year, month, day);
+            return string(temp);
         }
-        //~classTime(){};
-        void print(){ cout << week;}
 };
 
 
 //----------------------------student类，记录学生信息------------------------------------------------------------------
-class student
+class student : public date
 {
     private:
         char name[30];
         char sex;
         char id[11];
     public:
-        student(){};//生成空的学生类
-        student(const char *name, const char sex, const char id[11])
+        student(){}
+        student(const char *name, const char sex, const char id[11], date bd): date(bd)
+        {
+            strcpy(this->name, name);
+            this->sex = sex;
+            strcpy(this->id, id);
+        }
+        student(const char *name, const char sex, const char id[11], int y, int m, int d): date(y, m, d)
         {
             strcpy(this->name, name);
             this->sex = sex;
@@ -55,7 +48,7 @@ class student
         void setName(const char *name){ strcpy(this->name, name); }
         void setSex(char sex){ this->sex = sex; }
         void setId(const char id[11]){ strcpy(this->id, id); }
-        void print(){ cout << name << "\t" << sex << "\t" << id << endl; }
+        void print(){ cout << name << "\t" << sex << "\t" << id << "\t" << dob() << endl; }
 };
 
 //用于确认文件是否存在
@@ -77,9 +70,9 @@ void checkNameList()
         ofstream temp;
         temp.open("studentList.txt", ios_base::trunc);
         student s[3];
-        s[0] = student("常清华", 'F', "2020010001");
-        s[1] = student("林北大", 'M', "2020010002");
-        s[2] = student("复旦大", 'M', "2020010003");
+        s[0] = student("常清华", 'F', "2020010001", 2000, 1, 1);
+        s[1] = student("林北大", 'M', "2020010002", 2001, 2, 2);
+        s[2] = student("复旦大", 'M', "2020010003", 2010, 3, 3);
         temp.write((char*)&s[0], sizeof(s[0]));
         temp.write((char*)&s[1], sizeof(s[1]));
         temp.write((char*)&s[2], sizeof(s[2]));
@@ -94,7 +87,8 @@ int main()
     ifstream studentList;
     studentList.open("studentList.txt", ios::in);
     studentList.seekg(0);
-
-    
+    student s;
+    studentList.read((char*)&s, sizeof(s));
+    s.print();
     return 0;
 }
